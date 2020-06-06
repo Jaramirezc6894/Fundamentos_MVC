54c8520483554d7e752b9f377b46d987d83ccf87
Jaramirezc6894 <50533777+Jaramirezc6894@users.noreply.github.com>
2020-06-06 13:48:34 -0500

    Se integra una pequeña referencia sobre el modelado de datos en MVC, esta referencia se puede hacer desde SQL Server, Las clases que se integran son Restaurant y CusineType

---
+++ Fundamentos_MVC/Fundamentos_MVC/Models/CusineType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fundamentos_MVC.Models
+{
+    public enum CuisineType
+    {
+        None,
+        Italian,
+        Indian,
+        French
+    }
+}
