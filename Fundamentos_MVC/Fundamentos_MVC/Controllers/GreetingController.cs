ad5b350a8c02d133d1a6659b4d692ffe8e420c89
Jaramirezc6894 <50533777+Jaramirezc6894@users.noreply.github.com>
2020-06-06 15:51:11 -0500

    Se comienza con la creación del curso, comenzando con la creación de un nuevo controlador llamado GreetingController y una nueva vista llamada Index

---
+++ Fundamentos_MVC/Fundamentos_MVC/Controllers/GreetingController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fundamentos_MVC.Controllers
+{
+    public class GreetingController : Controller
+    {
+        // GET: Greeting
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Greeting/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Greeting/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Greeting/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Greeting/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Greeting/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Greeting/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Greeting/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
