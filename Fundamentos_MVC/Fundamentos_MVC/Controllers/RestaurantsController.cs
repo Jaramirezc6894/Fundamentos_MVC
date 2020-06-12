554fc69e03d8460173ebbbd67de4e106232f5fe4
Jaramirezc6894 <50533777+Jaramirezc6894@users.noreply.github.com>
2020-06-12 18:55:32 -0500

    Integracion de controlador y vista de elemento no encontrado, así como su actualización en la logica, tambien se implementea nueva logica y elementos al controlador de create asi como nueva estructura en la logica del modelo, tambien se impleneta el parte del controlador edit, de igual menera con logica de editar algun elemento, los controladores antes mencionados aun notienen la funcionalidad para poder guardas datos en la base de datos y tampoco se a aplicado logica de entity framework

--- Fundamentos_MVC/Fundamentos_MVC/Controllers/RestaurantsController.cs
+++ Fundamentos_MVC/Fundamentos_MVC/Controllers/RestaurantsController.cs
@@ -1,8 +1,5 @@
-﻿using Fundamentos_MVC.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Fundamentos_MVC.Models;
+using Fundamentos_MVC.Services;
 using System.Web.Mvc;
 
 namespace Fundamentos_MVC.Controllers
@@ -20,6 +17,7 @@ namespace Fundamentos_MVC.Controllers
             var model = db.GetAll();
             return View(model);
         }
+        [HttpGet]
         public ActionResult Details(int id)
         {
             var model = db.Get(id);
@@ -29,10 +27,53 @@ namespace Fundamentos_MVC.Controllers
             }
             return View(model);
         }
+        [HttpGet]
         public ActionResult Create()
         {
 
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+
+            //************Se reciclara esta logica de autentificacion de campos para asi implementar una nuevalogica
+            //if(String.IsNullOrEmpty(restaurant.Name))
+            //{
+            //    ModelState.AddModelError(nameof(restaurant.Name), "The name is required");
+            //}
+
+
+
+            if(ModelState.IsValid)
+            {            
+                db.Add(restaurant);
+                return RedirectToAction("Details", new {id = restaurant.Id});
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var model = db.Get(id);
+            if(model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Update(restaurant);
+                return RedirectToAction("Details", new { id = restaurant.Id });
+            }
+            return View(restaurant);
         }
     }
 }
--- Fundamentos_MVC/Fundamentos_MVC/Models/Restaurant.cs
+++ Fundamentos_MVC/Fundamentos_MVC/Models/Restaurant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,9 @@ namespace Fundamentos_MVC.Models
     public class Restaurant
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Display(Name="Type of food")]
         public CusineType Cuisine { get; set; }
 
     }
